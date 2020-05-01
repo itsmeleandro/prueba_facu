5e51cb22703ecaf626b4408a3806bd8e1b6b0d4b
leandro <rojasleandroismael617@gmail.com>
2020-05-01 05:29:54 -0300

    probe derivar a distintas paginas xd

--- src/js/stemen.js
+++ src/js/stemen.js
@@ -1,21 +1,32 @@
-$('#ingresar').click(
-    function(e){
-        let input = $('#usuario').val();
-        if (input.toUpperCase() === "INICIO") {
-            window.location.href = './inicio.html';
+document.querySelector('#ingresar').onclick = function(e){
+    var usuario = document.getElementById("usuario").value;
+    var contraseña = document.getElementById("contraseña").value;
+   
+     if (usuario === "usuario" && contraseña === "usuario") {
+        window.location.href = './inicio.html';
+        
         }
-        e.preventDefault()
-    }
-);
-$('#button-addon2').click(
-    function(){
-        let contrasenia = $('#contrasenia');
-        if(contrasenia.prop('type') === 'password'){
-            contrasenia.clone().prop('type','text').insertAfter('#contrasenia').prev().remove();
-        }else{
-            contrasenia.clone().prop('type','password').insertAfter('#contrasenia').prev().remove();
-        }
-    }
-);
+        if (usuario === "usuario2" && contraseña === "usuario2") {
+            window.location.href = './inicio2.html';
+            }
+            if (usuario === "usuario3" && contraseña === "usuario3") {
+                window.location.href = './inicio3.html';
+                }
+                if (usuario === "usuario4" && contraseña === "usuario4") {
+                    window.location.href = './inicio4.html';
+                    }
+                    if (usuario === "usuario5" && contraseña === "usuario5") {
+                        window.location.href = './inicio5.html';
+                        }
+                        if (usuario === "usuario6" && contraseña === "usuario6") {
+                            window.location.href = './inicio6.html';
+                            }
+                            if (usuario== "" || contraseña=="") {
+                                alert("verifique usuario o contraseña")
+                            }
+    
+    
+
     e.preventDefault()
+    
 }
