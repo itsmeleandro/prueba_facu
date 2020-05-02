f4c701483ccf035e1233a97cbb8bcbac20d0334c
leandro <rojasleandroismael617@gmail.com>
2020-05-02 02:20:25 -0300

    agregue compras y admin

--- src/js/stemen.js
+++ src/js/stemen.js
@@ -2,11 +2,16 @@ $('#ingresar').click(
     function(e){
         var usuario= document.getElementById("usuario").value;
         var contraseña= document.getElementById("contrasenia").value;
+        
         if (usuario == "ventas" && contraseña=="ventas") {
-            window.location.href= "./menuventas.html";
-            
-        }
+            window.location.href= "./menuventas.html"; }
+            if (usuario == "compras" && contraseña=="compras") {
+                window.location.href= "./menucompras.html"; }
+        
+        
+ 
+        
 
-                e.preventDefault()
-    }
+    e.preventDefault()
+}
 );
