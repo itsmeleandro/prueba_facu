07c9c85687de68af5a01e2ac984631643ded3021
marianoGnlz <marianoGnlz@users.noreply.github.com>
2020-05-04 19:35:26 -0300

    Modificacion del formulario de agregar cliente
    Se agrego una alerta de que el contenido fue completado.
    El input de telefono utiliza ahora una regex para comprobar validez

--- src/js/clientesAgregar.js
+++ src/js/clientesAgregar.js
@@ -3,27 +3,22 @@ function deleteAlert() {
         $('#alert').remove();
     }
 }
-function controlDeErrores(){
-    deleteAlert();
-    return controlDeValidacion();
-}
-
-function controlDeValidacion(){
-    return (
-        !$('#nombre')[0].checkValidity() || !$('#telefono')[0].checkValidity() || !$('#email')[0].checkValidity() 
+$('document').ready(
+    function(){
+        $('#registro').submit(
+            function (e) {
+                if($('#alert')){
+                    deleteAlert();
+                }
+                let alert = $(`
+                <div id='alert' class="alert alert-success" role="alert">
+                    Registro completado satisfactorimente.
+                </div>`);
+                $('#registro').before(alert);
+                setTimeout(deleteAlert, 3000);
+                e.preventDefault();
+                e.stopPropagation();
+            }
         )
-}
-$('#registrar').click(
-    function (e) {
-        if(controlDeErrores()){
-            console.log('hola')
-        }
-        let alert = $(`
-        <div id='alert' class="alert alert-success" role="alert">
-            Registro completado satisfactorimente.
-        </div>`);
-        $('#registro').before(alert);
-        setTimeout(deleteAlert, 3000)
-        e.preventDefault();
     }
 );
