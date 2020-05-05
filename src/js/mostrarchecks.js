7964f8ef8246cbe6c5204a9871f1a6ef810da3d3
EstefaniaPerez <EstefaniaPerez@users.noreply.github.com>
2020-05-05 16:17:06 -0300

    tipos de mat prima de cada proveedor

---
+++ src/js/mostrarchecks.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("#check1").change(function () {
+        if(this.checked){
+            $("#cont1").show();}
+            else{
+                $("#cont1").hide();
+            }
+    });
+
+    $("#check2").change(function () {
+        if(this.checked){
+            $("#cont2").show();}
+            else{
+                $("#cont2").hide();
+            }
+    });
+
+    $('#reigstrar').click(function(){
+        var selected = '';    
+        $('input[type=checkbox]').each(function(){
+            if (this.checked) {
+                selected += $(this).val()+', ';
+            }
+        }); 
+
+        if (selected == '') {
+            alert('Debes seleccionar al menos una opción.');
+            return false;  
+        }  
+        else
+            return true;
+    });         
+
+    
+});
