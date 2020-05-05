5df9078c40423624b5469ea92c06876ded5bbfdf
EstefaniaPerez <EstefaniaPerez@users.noreply.github.com>
2020-05-04 23:03:38 -0300

    opcion modificar deshabilitada si pedido esta en proceso o terminado

--- src/js/estados.js
+++ src/js/estados.js
@@ -1,12 +1,14 @@
-$('#dataTable tr').each(function() {
+$('#dataTable tr').each(function () {
     var estado = $(this).find("td").eq(4).html();
-    var boton= document.getElementById("deshab"); 
-    var boton2= document.getElementById("modificar"); 
-    if(estado=="En Proceso"){
-       boton.disabled=true;
-       boton2.disabled=true;
-    }  
-    else if(estado=="Terminado"){
-        boton2.disabled=true;
+    var boton = document.getElementById("deshab");
+    var boton2 = document.getElementById("modificar");
+    if (estado == "En Proceso") {
+        boton.disabled = true;
+        boton2.href = "#";
+       
+    }
+    else if (estado == "Terminado") {
+        boton2.href = "#";
+        
     }
 });
