7fe522eb3b57ef5db7c0d144a20c52691541b96c
EstefaniaPerez <EstefaniaPerez@users.noreply.github.com>
2020-11-08 17:16:57 -0300

    agregando algo

---
+++ TPI_v3/src/main/java/com/disenio/tpi/TpiV3Application.java
@@ -0,0 +1,13 @@
+package com.disenio.tpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpiV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpiV3Application.class, args);
+	}
+
+}
---
+++ TPI_v3/src/test/java/com/disenio/tpi/TpiV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disenio.tpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
