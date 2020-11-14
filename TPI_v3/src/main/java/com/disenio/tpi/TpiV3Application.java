063a5774db4d6c95d17972558863d6987bcb25bf
itsmeleandro <rojasleandroismael617@gmail.com>
2020-11-14 18:37:47 -0300

    clases random

--- TPI_v3/src/main/java/com/disenio/tpi/TpiV3Application.java
+++ TPI_v3/src/main/java/com/disenio/tpi/TpiV3Application.java
@@ -1,3 +1,4 @@
+
 package com.disenio.tpi;
 
 import org.springframework.boot.SpringApplication;
@@ -8,6 +9,33 @@ public class TpiV3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpiV3Application.class, args);
+		empleadoRRHH fabrica = fabrica new empleadoRRHH ();
+		
+		idpersona vendedor = fabrica.getpersona("vendedor");
+		
+		idpersona contador = fabrica.getpersona("contador");
+		
+		
 	}
 
+	
+}
+
+public interface persona {
+	private string nombre;
+	private string apellido;
+	private string dni;
+	
+}
+
+public class vendedor implements persona{
+	private string fechaContratacion;
+	
+		public persona() {
+			this.nombre=(null);
+			this.apellido=(null);
+			this.dni=(null);
+			
+		}
+				
 }
