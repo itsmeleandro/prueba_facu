67045006feb0393e19ce0b3aef4c1c99944ddc3d
itsmeleandro <rojasleandroismael617@gmail.com>
2020-11-14 21:35:15 -0300

    mas empleados uwu

--- TPI_v3/src/main/java/com/disenio/tpi/TpiV3Application.java
+++ TPI_v3/src/main/java/com/disenio/tpi/TpiV3Application.java
@@ -11,31 +11,37 @@ public class TpiV3Application {
 		SpringApplication.run(TpiV3Application.class, args);
 		empleadoRRHH fabrica = fabrica new empleadoRRHH ();
 		
-		idpersona vendedor = fabrica.getpersona("vendedor");
+		idPersona Vendedor = Fabrica.getPersona("vendedor");
 		
-		idpersona contador = fabrica.getpersona("contador");
+		idPersona JefeDeposito = Fabrica.getPersona("Jefe deposito");
+		idPersona EmpleadoCompras = Fabrica.getPersona("Empleado Compras");
+		idPersona EmpleadoComercializacion = Fabrica.getPersona("EmpleadoComercializacion ");
+		idPersona SecretarioAdministracion = Fabrica.getPersona("SeretarioAdministracion");
 		
 		
 	}
 
-	
 }
 
-public interface persona {
-	private string nombre;
-	private string apellido;
-	private string dni;
-	
-}
-
-public class vendedor implements persona{
-	private string fechaContratacion;
-	
-		public persona() {
-			this.nombre=(null);
-			this.apellido=(null);
-			this.dni=(null);
-			
+public class Fabrica {
+	public idpersona (string puesto);
+	if (puesto == "vendedor") {
+		return new Vendedor();
 		}
-				
+	if (puesto == "Jefe deposito") {
+		return new Recepcionista();
+		}
+	if (puesto == "Empleado compras") {
+		return new Recepcionista();
+		}
+	if (puesto == "Empleado comercializacion") {
+		return new Recepcionista();
+		}
+	if (puesto == "Secretario de Administracion") {
+		return new Recepcionista();
+		}
+	
 }
+
+
+
