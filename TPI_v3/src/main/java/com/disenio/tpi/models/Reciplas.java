2b25130075ba09bcc17bfbd803f9d5a5711d719d
EstefaniaPerez <EstefaniaPerez@users.noreply.github.com>
2020-11-16 21:46:27 -0300

    nada

--- TPI_v3/src/main/java/com/disenio/tpi/models/Reciplas.java
+++ TPI_v3/src/main/java/com/disenio/tpi/models/Reciplas.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Getter
 @Setter
 public class Reciplas {
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	private String direccion;
