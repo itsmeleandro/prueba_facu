394a5d27544d6ca4e65e4f4b9a656128d26c13b7
leandro <rojasleandroismael617@gmail.com>
2020-11-17 03:54:23 -0300

    insert en la BD

--- TPI_v3/src/main/java/com/disenio/tpi/ServingWebContentApplication.java
+++ TPI_v3/src/main/java/com/disenio/tpi/ServingWebContentApplication.java
@@ -1,23 +1,33 @@
 package com.disenio.tpi;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+// import java.sql.SQLException;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+
+
 
 
 public class ServingWebContentApplication {
 
-	public static void main(String[] args) {
+	public static  void main(String[] args) {
 		// String url = "jdbc:mysql://localhost:3306/disenio";
 		// String user = "root";
 		// String password = "12345";
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
 			DriverManager.getConnection("jdbc:mysql://localhost:3306/disenio","root","12345");
-
+			Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/disenio","root","12345");
 			System.out.println("Conexion okis");
 
+			Statement stmt = (Statement) conn.createStatement();
+			String query1 = "INSERT INTO empleado " + "VALUES (1, 'leandro')";
+			stmt.executeUpdate(query1);
 
 		} catch (ClassNotFoundException | SQLException e) {
 			System.out.println("Conexion no okis");
@@ -25,6 +35,9 @@ public class ServingWebContentApplication {
 
 		}
 	}
+
+
+
 }
 
 
